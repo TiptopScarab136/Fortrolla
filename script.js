499e2e43b1b6178f9880c9c7d46dfc2c5820d04b
TiptopScarab136 <66167491+TiptopScarab136@users.noreply.github.com>
2024-08-27 01:05:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const navPanel = document.getElementById('nav-panel');
+    navPanel.classList.toggle('active');
+});
